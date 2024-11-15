dce270db4453f06fcbc39cd9a6e9ab304cd533e3
Sang Woo Park <swp2@princeton.edu>
2024-11-14 19:31:10 -0600

    takens

---
+++ analysis_takens/analysis_hongkong_takens_acf_2d.R
@@ -0,0 +1,67 @@
+library(rstan)
+library(posterior)
+library(dplyr)
+library(tidyr)
+source("../script/script_data_hongkong_resp.R")
+source("../R/distfun.R")
+source("../R/takens.R")
+
+name <- c("adeno", "hmpv", "rvev", "rsv")
+realname <- c("adeno", "hmpv", "rvev", "rsv")
+
+reslist <- vector('list', length(name))
+
+for (i in 1:length(name)) {
+  print(i)
+  truedata <- data_hongkong_resp_scaled
+  
+  truedata_filter <- truedata %>%
+    filter(key==realname[i]) %>%
+    filter(!is.na(cases))
+  
+  logcases_pre <- log(filter(truedata_filter, year < 2020)$cases+1)
+  
+  acfout <- acf(logcases_pre, lag.max=(52*2), plot=FALSE)
+  
+  tau <- which(head(acfout$acf, -1) > 0 & tail(acfout$acf, -1) < 0)[1]
+  
+  tmp <- truedata_filter %>%
+    group_by(week) %>%
+    mutate(
+      meanlogcase=mean(log(cases+1)[year < 2020])
+    )
+  
+  takens_perturb <- takens(log(tmp$cases+1), d=2, tau=tau)
+  takens_unperturb <- takens(tmp$meanlogcase, d=2, tau=tau)
+  
+  distdata_takens <- distfun(
+    mat_perturb = takens_perturb,
+    mat_unperturb = takens_unperturb,
+    time=tail(tmp$year+tmp$week/52, -tau),
+    tbreak=2020,
+    out="all"
+  )
+  
+  maxt_takens <- distdata_takens$time[which.max(distdata_takens$dist)]
+  
+  lfit_takens <- lm(log(dist)~time, data=filter(distdata_takens, time >= maxt_takens))
+  
+  # pp_takens <- predict(lfit_takens, newdata=data.frame(time=seq(maxt_takens, 2040, by=0.1)))
+  
+  tmp$tau <- tau
+  tmp$maxt_takens <- maxt_takens
+  tmp$dist_takens <- c(rep(NA, tau), distdata_takens$dist)
+  tmp$intercept_takens <- coef(lfit_takens)[[1]]
+  tmp$resilience <- coef(lfit_takens)[[2]]
+  tmp$resilience_lwr <- confint(lfit_takens)[2,1]
+  tmp$resilience_upr <- confint(lfit_takens)[2,2]
+  
+  tmp$key <- realname[i]
+  
+  reslist[[i]] <- tmp
+}
+
+analysis_hongkong_takens_acf_2d <- reslist %>%
+  bind_rows
+
+save("analysis_hongkong_takens_acf_2d", file="analysis_hongkong_takens_acf_2d.rda")
---
+++ figure_takens/figure_takens_acf_2d.R
@@ -0,0 +1,87 @@
+library(tidyr)
+library(dplyr)
+library(ggplot2); theme_set(theme_bw(base_family = "Times"))
+library(egg)
+load("../analysis_takens/analysis_korea_takens_acf_2d.rda")
+load("../analysis_takens/analysis_hongkong_takens_acf_2d.rda")
+load("../analysis_takens/analysis_hongkong_piv_takens_acf_2d.rda")
+load("../analysis_takens/analysis_canada_takens_acf_2d.rda")
+
+summ_korea_takens_acf_2d <- analysis_korea_takens_acf_2d %>%
+  group_by(key) %>%
+  summarise(
+    resilience=resilience[1],
+    resilience_lwr=resilience_lwr[1],
+    resilience_upr=resilience_upr[1]
+  ) %>%
+  mutate(
+    country="Korea"
+  )
+
+summ_hongkong_takens_acf_2d <- analysis_hongkong_takens_acf_2d %>%
+  group_by(key) %>%
+  summarise(
+    resilience=resilience[1],
+    resilience_lwr=resilience_lwr[1],
+    resilience_upr=resilience_upr[1]
+  ) %>%
+  mutate(
+    country="Hong Kong",
+    key=factor(key,
+               levels=c("adeno", "hmpv", "rsv", "rvev"),
+               labels=c("Adenovirus", "Human metapneumovirus",
+                        "RSV", "Rhinovirus/Enterovirus"))
+  )
+
+summ_hongkong_piv_takens_acf_2d <- analysis_hongkong_piv_takens_acf_2d %>%
+  group_by(key) %>%
+  summarise(
+    resilience=resilience[1],
+    resilience_lwr=resilience_lwr[1],
+    resilience_upr=resilience_upr[1]
+  ) %>%
+  mutate(
+    country="Hong Kong",
+    key="Parainfluenza virus"
+  )
+
+summ_canada_takens_acf_2d <- analysis_canada_takens_acf_2d %>%
+  group_by(key) %>%
+  summarise(
+    resilience=resilience[1],
+    resilience_lwr=resilience_lwr[1],
+    resilience_upr=resilience_upr[1]
+  ) %>%
+  mutate(
+    country="Canada",
+    key=factor(key,
+               levels=c("AdV", "CoV", "Flu A", "Flu B", "HMPV", "PIV", "RSV", "RV/EV"),
+               labels=c("Adenovirus", "Influenza A", "Influenza B", "Human coronavirus",
+                        "Human metapneumovirus",
+                        "Parainfluenza virus",
+                        "RSV", "Rhinovirus/Enterovirus"))
+  )
+
+
+summ_resilience_acf_2d <-
+  bind_rows(
+    summ_korea_takens_acf_2d,
+    summ_hongkong_takens_acf_2d,
+    summ_hongkong_piv_takens_acf_2d,
+    summ_canada_takens_acf_2d
+  )
+
+g1 <- ggplot(summ_resilience_acf_2d) +
+  geom_point(aes(key, -resilience, col=country),
+             position=position_dodge(width=0.5)) +
+  geom_errorbar(aes(key, ymin=-resilience_lwr, ymax=-resilience_upr, col=country), width=0,
+                position=position_dodge(width=0.5)) +
+  scale_color_viridis_d("Country", end=0.8) +
+  scale_y_continuous("Resilience (1/years)") +
+  theme(
+    axis.text.x = element_text(angle=45, hjust=1),
+    strip.background = element_blank(),
+    legend.position = "top"
+  )
+
+ggsave("figure_takens_acf_2d.pdf", g1, width=8, height=6)
