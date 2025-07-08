1b964312ac16c062cb806805eea8799033a5b457
Sang Woo Park <swp2@princeton.edu>
2025-07-08 13:03:57 -0500

    clean up

--- analysis_takens_acf_fnn/analysis_nrevss_takens_acf_fnn.R
+++ analysis_takens_acf_fnn/analysis_nrevss_takens_acf_fnn.R
@@ -53,20 +53,9 @@ for (i in 1:length(realname)) {
     dist=dist
   )
   
-  maxt_takens <- distdata_takens$time[which.max(distdata_takens$dist)]
-  
-  lfit_takens <- lm(log(dist)~time, data=filter(distdata_takens, time >= maxt_takens))
-  
-  # pp_takens <- predict(lfit_takens, newdata=data.frame(time=seq(maxt_takens, 2040, by=0.1)))
-  
   tmp$tau <- tau
-  tmp$maxt_takens <- maxt_takens
   tmp$dist_takens <- c(rep(NA, tau*(d-1)), distdata_takens$dist)
   tmp$d <- d+1
-  tmp$intercept_takens <- coef(lfit_takens)[[1]]
-  tmp$resilience <- coef(lfit_takens)[[2]]
-  tmp$resilience_lwr <- confint(lfit_takens)[2,1]
-  tmp$resilience_upr <- confint(lfit_takens)[2,2]
   
   reslist[[i]] <- tmp
 }
